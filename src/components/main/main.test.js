86830dd3930a962040ce3540be53a19ab820ffe8
unknown <mainrub06@gmail.com>
2020-02-26 18:59:27 +0300

    module5-task1 reducer test(not complete)

--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -6,7 +6,7 @@ import PLACE_MOCK from "../../mock/offers.js";
 it(`Render Main`, () => {
   const tree = renderer
     .create(
-        <Main onOfferClick={() => {}} dataCards={PLACE_MOCK} />
+        <Main onChangeCity={() => {}} getCityOffers={() => {}} onOfferClick={() => {}} dataCards={PLACE_MOCK} />
     )
     .toJSON();
 
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,1 +1,17 @@
-import {reducer, ActionCreator} from "./reducer.js";
+import {reducer} from "./reducer.js";
+import mockCityOffers from "./mock/city-offers";
+import mockCityOffersDetail from "./mock/city-offers-detail";
+import mockReviews from "./mock/reviews";
+
+const initialCity = Object.keys(mockCityOffers)[0];
+const initialCityOffers = mockCityOffers[initialCity];
+const initialCityOffersDetail = mockCityOffersDetail[initialCity];
+
+it(`Reducer return initial state`, () => {
+  expect(reducer(undefined, {})).toEqual({
+    city: initialCity,
+    offers: initialCityOffers,
+    offersDetail: initialCityOffersDetail,
+    reviews: mockReviews
+  });
+});
